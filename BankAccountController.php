f8a9e06fcb021d5da2420446bba36b96e3e3696c
hinnurainen <72872684+hinnurainen@users.noreply.github.com>
2021-05-18 14:47:57 +0300

    Update BankAccountController.php

--- BankAccountController.php
+++ BankAccountController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Account;
 use App\Bank;
+use App\key;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,24 +17,37 @@ class BankAccountController extends AbstractController
     public function index(): Response
     {
         $bank = new Bank();
+
+        $firstKey = new key(789);
         $firstAccount = new Account(1000, 12345);
+
+        $secondKey = new key(147);
         $secondAccount = new Account(5000, 123);
+
+        $thirdKey = new key(456);
         $thirdAccount = new Account(10000, 9999);
 
         $bank->addAccount($firstAccount);
         $bank->addAccount($secondAccount);
         $bank->addAccount($thirdAccount);
 
-        $bank->getAccountById(9999)->deposit(-1000); // illegal
-        $bank->getAccountById(9999)->deposit(1000);
+        $mortgagePayment = new MortgagePayment($firstAccount);
+        $mortgagePayment->makePayment(500);
 
-        $bank->getAccountById(123)->withdraw(-10000); // illegal
-        $bank->getAccountById(123)->withdraw(10000); // legal, not possible
-        $bank->getAccountById(123)->withdraw(1000); // legal, should deduct 1000
+        return $this->json([
+            'key comparison' => $firstKey->equals(new key(789))
+        ]);
+    }
+}
+   /*    $bank->getAccountById(9999)->deposit(-1000); // illegal
+         $bank->getAccountById(9999)->deposit(1000);
+         $bank->getAccountById(123)->withdraw(-10000); // illegal
+         $bank->getAccountById(123)->withdraw(10000); // legal, not possible
+         $bank->getAccountById(123)->withdraw(1000); // legal, should deduct 1000
 
         return $this->json([
             'bank_id' => 12345,
             'balance' => $bank->getAccountById(12345)->getBalance(),
         ]);
     }
-}
+}*/
